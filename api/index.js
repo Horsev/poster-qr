cf91764de15bd02e049a4a9fd1c288f9fbee1e0a
Horsev <andrey.horsev@gmail.com>
2024-03-11 12:50:24 +0200

    Fetch client endpoint

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import { faIcon, noNewline, uglifyJS } from "../helpers/pug-filters.js";
 import getPageData from "../data/index.js";
 
+const clientID = "pubkedsconfig";
+
 const createCompanyPage = async (request, result) => {
   const filters = {
     "uglify-js": uglifyJS,
@@ -8,24 +10,15 @@ const createCompanyPage = async (request, result) => {
     "fa-icon": faIcon,
   };
 
-  let templateData = {};
+  const pageData = await getPageData(clientID);
 
-  try {
-    const pageData = await getPageData();
-    templateData = {
-      ...pageData,
-      filters,
-    };
-  } catch ({ message }) {
-    templateData = {
-      error: true,
-      message: `Помилка: ${message}`,
-      filters,
-    };
-  } finally {
-    const template = templateData.error ? "error.pug" : "index.pug";
-    result.render(template, templateData);
-  }
+  const templateData = {
+    ...pageData,
+    filters,
+  };
+
+  const template = templateData.isError ? "error.pug" : "index.pug";
+  result.render(template, templateData);
 };
 
 export default createCompanyPage;
--- data/index.js
+++ data/index.js
@@ -1,9 +1,4 @@
-const settings = {
-  logo: "/img/keds512.webp",
-  name: "Паб Кеди",
-  table: 30,
-  url: "http://ps.me",
-};
+const getEndpoint = (id) => `https://assets.codepen.io/1959313/${id}.json`;
 
 const menu = [
   { name: "Меню", icon: "book-open-cover" },
@@ -19,9 +14,21 @@ const menu = [
   },
 ];
 
-const getPageData = async () => ({
-  settings,
-  menu,
-});
+const getPageData = async (clientID) => {
+  const ENDPOINT = getEndpoint(clientID);
+  try {
+    const response = await fetch(ENDPOINT);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const settings = await response.json();
+
+    return { settings, menu };
+  } catch ({ message }) {
+    return { message, isError: true };
+  }
+};
 
 export default getPageData;
